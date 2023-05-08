0394738f95b89a1d2b83b66640969539864bb978
Joshua de Lange <joshua.delange@gmail.com>
2023-05-08 14:57:40 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Testing app</h1>
+  <NewsletterSignUp />
+</template>
---
+++ components/newsletter-sign-up.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    const email = useState('email')
+
+    async function onFormSubmit(event: Event) {
+        event.preventDefault()
+
+        const response = await $fetch('/api/sign-up-newsletter')
+        console.log(response)
+    }
+</script>
+
+<template>
+    <form @submit="onFormSubmit">
+        <h2>Newsletter Sign Up</h2>
+        <p>Enter your email address to receive updates.</p>
+        <input type="email" v-model="email" />
+        <button>Sign Up</button>
+    </form>
+</template>
